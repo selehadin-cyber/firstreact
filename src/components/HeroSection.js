1023694bc776b0d9b185ef9d20b8129237c3d39f
selehaddin <sele4416@gmail.com>
2021-11-28 14:15:24 +0300

    background changes and other improvements

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,14 +6,15 @@ import "./HeroSection.css";
 function HeroSection() {
     return (
         <div className="hero-container">
-            <video src="/videos/video-2.mp4" autoPlay loop muted />
-            <h1>Macera Geliyor</h1>
-            <p>Neyi bekliyorsun?</p>
-            <div className="hero-btns">
+            <video src="videos/video-1.mp4" autoPlay loop muted />
+            {/*<iframe width="560" height="315" src="https://www.youtube.com/embed/2EyIEcyQ-0A?autoplay=1&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
+            {/*<h1>Macera Geliyor</h1>*/}
+             <p>Gökpusu!</p> 
+            {/*<div className="hero-btns">
                 <Button className="btns" buttonStyle="btn--outline" buttonSize="btn--large">BAŞLA</Button>
                 <Button className="btns" buttonStyle="btn--primary" buttonSize="btn--large">Videolarımız <i className="far fa-play-circle" /></Button>
                 
-            </div>
+            </div>*/}
         </div>
     )
 }
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -2,5 +2,11 @@ import react from "react";
 import "../App.css"
 
 export default function Services() {
-    return <h1 className="services">SERVICES</h1>;
+    return ( <>
+            <div className="services">
+                <h1>HAKKMIZDA</h1>
+                <p >Gökpusu Takımı, Pamukkale Üniversite’sinde eğitim gören Mühendislik öğrencileri tarafından kurulmuştur. Kuruluş amacı, Hava araçları sahasında araştırmalar yapmak teorik bilgilerimizi pekiştirerek insansız hava araçları sistemleri üzerinde çalışmaktır. İnsansız hava araçlarını irdelemek, geliştirmek ve yeni teknolojik birikimleri kullanarak donanımlarını arttırarak özgün ürünler ortaya koymaktır.</p>
+            </div>
+            </>
+            )
 }
