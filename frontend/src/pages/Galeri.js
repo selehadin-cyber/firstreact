5a750b0e29b4ccbcd51565439e2d76be88fa499c
selehadin-cyber <sele4416@gmail.com>
2022-01-31 15:23:09 +0300

    video added

--- frontend/src/pages/Galeri.js
+++ frontend/src/pages/Galeri.js
@@ -1,4 +1,5 @@
 import "../App.css"
+import "./gallery.css"
 import CardItem from "../components/CardItem";
 import Cards from "../components/Cards";
 import "../components/Cards.css"
@@ -7,6 +8,13 @@ export default function Galeri() {
     return (
         <div className='cards'>
         <h1>Galeri</h1>
+        <h2>videolar</h2>
+        <video width="320px" height="240px" src="videos/video1.mp4" controls>
+          <source src="videos/video1.mp4" type="video/mp4"/>
+        </video>
+        <iframe width="420" height="315"
+          src="https://www.youtube.com/embed/ai6zLXuzvI8">
+        </iframe> 
         <div className="container">
           <div className="project"><img src="images/gp1.jpg" className="project-image" alt="" /></div>
           <div className="project"><img src="images/gp2.jpg" className="project-image" alt="" /></div>
