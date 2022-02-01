52625fbd7899a9e9b2957ec7f02528a37190e56c
selehadin-cyber <sele4416@gmail.com>
2022-02-01 16:45:31 +0300

    centered video element

--- frontend/src/pages/Galeri.js
+++ frontend/src/pages/Galeri.js
@@ -8,13 +8,15 @@ export default function Galeri() {
     return (
         <div className='cards'>
         <h1>Galeri</h1>
-        <h2>videolar</h2>
+        <h1>videolar</h1>
         <video width="320px" height="240px" src="videos/video1.mp4" controls>
           <source src="videos/video1.mp4" type="video/mp4"/>
         </video>
-        <iframe width="420" height="315"
-          src="https://www.youtube.com/embed/ai6zLXuzvI8">
-        </iframe> 
+        <div className="vidcon">
+          <iframe width="420" height="315"
+            src="https://www.youtube.com/embed/ai6zLXuzvI8">
+          </iframe>
+        </div> 
         <div id="teknofest">
           <h1>teknofest</h1>
           <div className="container">
